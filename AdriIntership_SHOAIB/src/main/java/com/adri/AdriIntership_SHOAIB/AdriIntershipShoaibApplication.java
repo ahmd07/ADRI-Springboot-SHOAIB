6270a652c8ef5c1b64fc440395916369dff5c0f7
ahmd07 <69446354+ahmd07@users.noreply.github.com>
2023-02-05 21:26:20 +0300

    Add files via upload

---
+++ AdriIntership_SHOAIB/src/main/java/com/adri/AdriIntership_SHOAIB/AdriIntershipShoaibApplication.java
@@ -0,0 +1,13 @@
+package com.adri.AdriIntership_SHOAIB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdriIntershipShoaibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdriIntershipShoaibApplication.class, args);
+	}
+
+}
---
+++ AdriIntership_SHOAIB/src/test/java/com/adri/AdriIntership_SHOAIB/AdriIntershipShoaibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adri.AdriIntership_SHOAIB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdriIntershipShoaibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
